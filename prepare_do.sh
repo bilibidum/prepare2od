87aa457aa66bf5160c0d3476cc6d9e2ca6a5a08e
Rafael Bilibidum <bilibidum@gmail.com>
2016-10-10 01:58:43 -0300

    script de configuração
    ver 1.9.2

--- prepare_do.sh
+++ prepare_do.sh
@@ -38,7 +38,34 @@ iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8069
 
 sudo dpkg-reconfigure tzdata
 
+echo -e "\n--- Atualizando o sistema ---" 
+sudo apt-get update
+sudo apt-get dist-upgrade
+sudo apt-get install python-dev python-yaml python-feedparser python-geoip python-imaging python-pybabel python-unicodecsv wkhtmltopdf libxml2-dev libxmlsec1-dev python-argparse python-Babel python-cups python-dateutil python-decorator python-docutils python-feedparser python-gdata pythongevent python-greenlet python-Jinja2 python-libxslt1 python-lxml python-Mako python-MarkupSafe python-mock python-openid python-passlib pythonpsutil python-psycopg2 python-pychart python-pydot python-pyparsing pythonpyPdf python-ldap python-yaml python-reportlab python-requests pythonsimplejson python-six python-tz python-unittest2 python-vatnumber pythonvobject python-webdav python-Werkzeug python-wsgiref python-xlwt python-zsi python-dev libpq-dev poppler-utils python-pdftools antiword
+sudo apt-get install python-pip 
+sudo apt-get install python-setuptools 
+sudo pip install pyserial==2.7
+sudo pip install psycogreen==1.0
+sudo pip install pyusb==1.0.0b1
+sudo pip install qrcode==5.0.1
+sudo apt-get install python-dev
+sudo pip install Pillow==2.5.1
+sudo pip install boto==2.38.0
+sudo pip install oerplib==0.8.4
+sudo pip install jcconv==0.2.3
+sudo pip install pytz==2014.4
+sudo pip install suds
+sudo pip install suds_requests
 
+echo -e "\n--- Instalando o wkhtmltopdf ---" 
+cd /tmp
+wget http://download.gna.org/wkhtmltopdf/0.12/0.12.2/wkhtmltox-0.12.2_linux-trusty-amd64.deb
+sudo dpkg -i wkhtmltox-0.12.2_linux-trusty-amd64.deb
+sudo apt-get install -f
+sudo dpkg -i wkhtmltox-0.12.2_linux-trusty-amd64.deb
+sudo cp /usr/local/bin/wkhtmltopdf /usr/bin
+sudo cp /usr/local/bin/wkhtmltoimage /usr/bin
+wkhtmltopdf -–version
 
 echo -e "\n--- Criando SWAP : 2gb ---" 
 
@@ -57,6 +84,6 @@ sudo rm -R prepare2odoo
 echo -e "\n "
 echo -e "\n "
 echo -e "\n "
-echo -e "\033[01;31m   CONFIGURAÇAO REALIZADA COM SUCESSO\033[01;37m"
+echo -e "n----- CONFIGURAÇAO REALIZADA COM SUCESSO -----"
 echo -e "\n "
 echo -e "\n "
