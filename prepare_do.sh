46994315f87ea6a17294ef633c520c9d86a0ebd8
Rafael Bilibidum <bilibidum@gmail.com>
2016-10-28 02:05:08 -0200

    Update prepare_do.sh

--- prepare_do.sh
+++ prepare_do.sh
@@ -41,14 +41,14 @@ figlet -f term "Feito!"
 figlet -f term "##### Atualizando o sistema #####"
 sudo apt-get update
 sudo apt-get dist-upgrade
-sudo apt-get install python-dev python-yaml python-feedparser python-geoip python-imaging python-pybabel python-unicodecsv wkhtmltopdf libxml2-dev libxmlsec1-dev python-argparse python-Babel python-cups python-dateutil python-decorator python-docutils python-feedparser python-gdata pythongevent python-greenlet python-Jinja2 python-libxslt1 python-lxml python-Mako python-MarkupSafe python-mock python-openid python-passlib pythonpsutil python-psycopg2 python-pychart python-pydot python-pyparsing pythonpyPdf python-ldap python-yaml python-reportlab python-requests pythonsimplejson python-six python-tz python-unittest2 python-vatnumber pythonvobject python-webdav python-Werkzeug python-wsgiref python-xlwt python-zsi python-dev libpq-dev poppler-utils python-pdftools antiword
-sudo apt-get install python-pip 
-sudo apt-get install python-setuptools 
+sudo apt-get install -y python-dev python-yaml python-feedparser python-geoip python-imaging python-pybabel python-unicodecsv wkhtmltopdf libxml2-dev libxmlsec1-dev python-argparse python-Babel python-cups python-dateutil python-decorator python-docutils python-feedparser python-gdata pythongevent python-greenlet python-Jinja2 python-libxslt1 python-lxml python-Mako python-MarkupSafe python-mock python-openid python-passlib pythonpsutil python-psycopg2 python-pychart python-pydot python-pyparsing pythonpyPdf python-ldap python-yaml python-reportlab python-requests pythonsimplejson python-six python-tz python-unittest2 python-vatnumber pythonvobject python-webdav python-Werkzeug python-wsgiref python-xlwt python-zsi python-dev libpq-dev poppler-utils python-pdftools antiword
+sudo apt-get install -y python-pip 
+sudo apt-get install -y python-setuptools 
 sudo pip install pyserial==2.7
 sudo pip install psycogreen==1.0
 sudo pip install pyusb==1.0.0b1
 sudo pip install qrcode==5.0.1
-sudo apt-get install python-dev
+sudo apt-get install -y python-dev
 sudo pip install Pillow==2.5.1
 sudo pip install boto==2.38.0
 sudo pip install oerplib==0.8.4
